9e18f76015038ce7c7d37564dcfa9697576d6d8e
Sheila Anguiano <sheila.anguiano@gmail.com>
2020-01-09 11:46:11 -0800

    First commit

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('<h1>Hello World via Express!!!</h1>');
+});
+
+app.get('/hello', (req, res) => {
+    res.send('<h1>Hello JavaScript Developer!!!</h1>');
+});
+
+app.listen(3000, () => {
+    console.log('The application is running on localhost:3000!')
+});
