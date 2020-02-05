ef423f42e649d194b3f95813b598636d21a13021
Sheila Anguiano <sheila.anguiano@gmail.com>
2020-02-05 13:49:28 -0800

    UX improvements

--- routes/cards.js
+++ routes/cards.js
@@ -4,10 +4,22 @@ const { data } = require('../data/flashcardData.json');
 const { cards } = data;
 
 router.get('/:id', (req, res) => {
-    res.render('card', { 
-        prompt: cards[req.params.id].question,
-        hint: cards[req.params.id].hint
-    });
+    const { side } = req.query;
+    const { id } = req.params;
+    const text = cards[id][side];
+    const { hint } = cards[id];
+    const templateData = { id, text };
+
+    if( side === 'question'){
+        templateData.hint = hint;
+        templateData.sideToShow = 'answer';
+        templateData.sideToShowDisplay= 'Answer';
+    } else if (side === 'answer'){
+        templateData.sideToShow = 'question';
+        templateData.sideToShowDisplay = 'Question';
+    }
+    
+    res.render('card', templateData);
 });
 
 module.exports = router;
